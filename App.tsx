5c0cb3e2815f8cba1be21de2adfd2269014e8f5e
cirogithub1 <cirosequea.fr@gmail.com>
2023-06-22 00:12:39 +0200

    220623
    Tailwind ok

--- App.tsx
+++ App.tsx
@@ -3,8 +3,10 @@ import { SafeAreaView, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className='text-red-500 text-3xl bg-blue-500'>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className='bg-blue-500'>
+      <Text
+        className='text-red-500'>
+          Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   )
